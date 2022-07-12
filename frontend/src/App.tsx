f4fb227cdc26101a961b3fac978902c31cdae7ab
Leandro Amorim <leandroamorimpb@gmail.com>
2022-07-12 18:45:24 -0300

    Other components :beetle:

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,5 @@
+import Header from "./components/Header"
+import CardVendas from "./components/CardVendas"
 import NotificationButton from "./components/NotificationButton"
 
 /**Declaração de função do javascript */
@@ -5,8 +7,14 @@ function App() {
   /**O nome disso '<></>' é fragmated */
   return (
     <>
-      <h1>Olá Mundo</h1>
-      <NotificationButton />
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <CardVendas />
+          </div>
+        </section>
+      </main>
     </>
   )
 }
---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,18 @@
+import logo from '../../assets/img/header.svg'
+import './styles.css'
+function Header() {
+    return (
+        <header>
+            <div className="dsmeta-logo-container">
+                <img src={logo} alt="ArfaxTechSoft" />
+                <h1>ArfaxTechSoft</h1>
+                <p>
+                    Desenvolvido por
+                    <a href="https://www.linkedin.com/in/leandro-amorim-batista/">Leandro Amorim</a>
+                </p>
+            </div>
+        </header>
+    )
+}
+
+export default Header
