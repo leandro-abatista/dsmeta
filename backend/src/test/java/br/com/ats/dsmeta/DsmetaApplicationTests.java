32ac1f35f75bd60351b13f0e62bcfd4767d6350d
Leandro Amorim <leandroamorimpb@gmail.com>
2022-07-12 15:54:07 -0300

    Project Creat :unlock:

---
+++ backend/src/test/java/br/com/ats/dsmeta/DsmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ats.dsmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
