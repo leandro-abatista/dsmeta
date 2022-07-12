d226e15577322125093f61e30d75159a8e36a6ef
Leandro Amorim <leandroamorimpb@gmail.com>
2022-07-12 16:19:16 -0300

    Project clean :beetle:

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+//Estrutura básica de aplicação react
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+
     <App />
   </React.StrictMode>
 )
