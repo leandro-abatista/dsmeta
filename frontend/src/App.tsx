d625bc4c7ccbb35830cc91f17bb0504ee9ed1a13
Leandro Amorim <leandroamorimpb@gmail.com>
2022-07-12 18:09:30 -0300

    First component :beetle:

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,13 @@
+import NotificationButton from "./components/NotificationButton"
 
 /**Declaração de função do javascript */
 function App() {
+  /**O nome disso '<></>' é fragmated */
   return (
-    <h1>Olá Mundo</h1>
+    <>
+      <h1>Olá Mundo</h1>
+      <NotificationButton />
+    </>
   )
 }
 
